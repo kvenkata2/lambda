d83ef7a13056c216b7b57bf294b095a54247de26
root <root@ip-172-31-6-246.ap-southeast-1.compute.internal>
2023-07-22 08:06:08 +0000

    first commit

---
+++ kvg.py
@@ -0,0 +1,5 @@
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import A4
+from PyPDF2 import PdfFileReader, PdfFileWriter
